4ccda44b734af5195c567702f734c5f53aff1149
Bas Stottelaar <basstottelaar@gmail.com>
2020-09-03 00:06:32 +0200

    Add create access point example application

---
+++ cmd/createap/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/theojulienne/go-wireless"
+)
+
+func main() {
+	var iface string
+	flag.StringVar(&iface, "i", "", "interface to use")
+	flag.Parse()
+
+	if iface == "" {
+		var ok bool
+		iface, ok = wireless.DefaultInterface()
+		if !ok {
+			panic("no wifi cards on the system")
+		}
+	}
+	fmt.Printf("Using interface: %s\n", iface)
+
+	wc, err := wireless.NewClient(iface)
+	if err != nil {
+		panic(err)
+	}
+	defer wc.Close()
+
+	net, err := wc.Create(wireless.NewAccessPoint(os.Args[1], os.Args[2]))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Created access point " + net.SSID)
+}
